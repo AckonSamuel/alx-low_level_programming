2cdb6d45cb1b2b935325e4e96755d6ba68063485
AckonSamuel <ackons433@gmail.com>
2022-03-11 04:39:04 +0000

    fix linter error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 /**
-
- * main - Wows
-
  *
-
+ * main - Wows
+ *
+ *
+ *
  * Return: Always 1 (Error)
-
+ *
  */
 
 #include<stdio.h>
@@ -13,13 +13,9 @@
 #include <unistd.h>
 
 int main(void)
-  
-{
-  
-  write(2, "and that piece of art is useful\" - Dora Korpar, \
 
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, \
 2015-10-19\n", 59);
-  
-  return (1);
-  
+return (1);
 }
