2b93b94dc9f0aa71b140c428d95fc03a242887d4
AckonSamuel <ackons433@gmail.com>
2022-03-11 04:27:25 +0000

    fix linter errors

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,29 +1,22 @@
 /**
-
- * main - Entry point
-
  *
-
+ * main - Entry point
+ *
+ *
+ *
  * Return: Always 0 (Success)
-
+ *
  */
 
 #include <stdio.h>
 
 int main(void)
-  
+
 {
-  
-  printf("Size of a char: %i byte(s)\n", (int) sizeof(char));
-  
-  printf("Size of an int: %i byte(s)\n", (int) sizeof(int));
-  
-  printf("Size of a long int: %i byte(s)\n", (int) sizeof(long int));
-  
-  printf("Size of a long long int: %i byte(s)\n", (int) sizeof(long long int));
-  
-  printf("Size of a float: %i byte(s)\n", (int) sizeof(float));
-  
+  printf("Size of a char: %i byte(s)\n", (int)sizeof(char));
+  printf("Size of an int: %i byte(s)\n", (int)sizeof(int));
+  printf("Size of a long int: %i byte(s)\n", (int)sizeof(long int));
+  printf("Size of a long long int: %i byte(s)\n", (int)sizeof(long long int));
+  printf("Size of a float: %i byte(s)\n", (int)sizeof(float));
   return (0);
-  
 }
