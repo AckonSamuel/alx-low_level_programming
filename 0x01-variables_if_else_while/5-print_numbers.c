1c4c582876aa82481b363d22cf8c6a3b76e054af
AckonSamuel <ackons433@gmail.com>
2022-03-12 02:25:17 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes here */
+
+/**
+ * main - This prints the alphabets in lowercase.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int lwch;
+
+
+	for (lwch = 0; lwch <= 9; lwch++)
+		putchar(lwch);
+	putchar('\n');
+	return (0);
+}
