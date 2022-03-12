f74032bc3cf6f5cba31456f5d1e1019051525f97
AckonSamuel <ackons433@gmail.com>
2022-03-12 01:43:49 -0800

    Print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes here */
+
+/**
+ * main - This prints the alphabets in lowercase and uppercase.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char lwch;
+
+
+	for (lwch =  'a'; lwch <= 'z'; lwch++)
+		putchar(lwch);
+	for (lwch = 'A'; lwch <= 'Z'; lwch++)
+		putchar(lwch);
+	putchar('\n');
+	return (0);
+}
