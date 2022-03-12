15250f6605e617986a7a796a58781fa930784840
AckonSamuel <ackons433@gmail.com>
2022-03-12 18:04:42 +0000

    print two two digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes here */
+
+/**
+ * main - This prints possible combinations of two two digits.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int lwch;
+	int j;
+	int i;
+	int s;
+
+	for (lwch = 0; lwch <= 9; lwch++)
+	{
+	for (j = 0; j <= 9; j++)
+	{
+	for (i = 0; i <= 9; i++)
+	{
+	for (s = j + 1; s <= 9; s++)
+	{
+	putchar(lwch + '0');
+	putchar(j + '0');
+	putchar(i + '0');
+	putchar(s + '0');
+	if (j != 8)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
