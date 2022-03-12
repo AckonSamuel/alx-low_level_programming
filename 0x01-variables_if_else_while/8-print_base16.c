6d71b9ef7e675b33cc4de9f2e1e8ee55e5c07ebf
AckonSamuel <ackons433@gmail.com>
2022-03-12 13:44:01 +0000

    print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes here */
+
+/**
+ * main - This prints the alphabets in lowercase.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int lwch;
+
+
+	for (lwch = 0; lwch <= 9; lwch++)
+	putchar(lwch, "0");
+	for (lwch = 'a'; lwch <= 'f'; lwch++)
+	putchar(lwch);
+	putchar('\n');
+	return (0);
+}
