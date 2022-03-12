97f2e615c7cd26a2b77045c4e610b7c26f281e8b
AckonSamuel <ackons433@gmail.com>
2022-03-12 02:16:44 -0800

    up

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes here */
+
+/**
+ * main - This prints the alphabets in lowercase.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char lwch;
+
+
+	for (lwch =  'a'; lwch <= 'z'; lwch++)
+	{
+		if ( lwch = 'e' || lwch = 'q')
+			lwch++;
+		putchar(lwch);
+	}
+	putchar('\n');
+	return (0);
+}
