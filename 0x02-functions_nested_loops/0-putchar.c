8619c1b8a1f8ebbc17688758ba8da33d25fc7034
AckonSamuel <ackons433@gmail.com>
2022-03-16 01:14:50 +0000

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - This prints the string _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	putchar("_putchar");
+	return (0);
+}
