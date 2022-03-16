1f5a728db14430ec55a966ef76580b76c3a2591a
AckonSamuel <ackons433@gmail.com>
2022-03-16 04:55:16 +0000

    Hpap

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _islower - to check for lowercase alphabets
+ *
+ * Return: 1 (Succes) or 0 (Fail)
+ */
+
+int _islower(int c)
+{
+	char first;
+	int count;
+	int sen = 0;
+	
+	for (first = 'a'; first <= 'z'; first++)
+	{
+		if (c == first)
+			sen++;
+		else
+			sen = sen;
+	}
+	return sen;
+}
